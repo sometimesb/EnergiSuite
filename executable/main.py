0f04ee3fc4bf2836a027f84f5f845e3f854d19d2
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-02-20 14:49:14 -0600

    need to matchup prices now, saving before i test new stuff

--- executable/main.py
+++ executable/main.py
@@ -3,35 +3,53 @@ from mapping import NAME_ID_MAPPING
 import json
 
 class Asset:
-    def __init__(self, name, symbol, price, cgID=None):
+    def __init__(self, name, symbol, price, cgID=None,cgPrice=None):
         self.name = name
         self.symbol = symbol
         self.price = price
         self.cgID = cgID
+        self.cgPrice = cgPrice
 
     def __str__(self):
         return f"{self.name} {self.cgID} {self.price}"
 
-@request(use_stealth=True, output="energi")
-def EnergiRequester(request: AntiDetectRequests, data):
-    response = request.get("https://api.energiswap.exchange/v1/assets")
-    if response.status_code != 200:
-        return "Error"
-    return response.text
+def apiRequester(url,mode):
+    @request(use_stealth=True, output="energi")
+    def EnergiRequester(request: AntiDetectRequests, data):
+        response = request.get(url)
+        if response.status_code != 200:
+            return "Error"
+        return response.text
+    
+    @request(use_stealth=True, output="coingecko")
+    def CoinGeckoRequester(request: AntiDetectRequests, data):
+        response = request.get(url)
+        if response.status_code != 200:
+            return "Error"
+        return response.text
+
+    if mode == 0:
+        return EnergiRequester()
+    elif mode == 1:
+        return CoinGeckoRequester()
+
+def parseCoinGeckoData():
+    data = json.loads(apiRequester("https://api.coingecko.com/api/v3/simple/price?ids=energi%2Cenergi-dollar%2Cdai%2Cethereum%2Cbitcoin%2Cusd-coin&vs_currencies=usd",1))
+    print(data)
 
 def parseEnergiData():
-    data = json.loads(EnergiRequester())
+    data = json.loads(apiRequester("https://api.energiswap.exchange/v1/assets",0))
     assets = []
     for key, value in data.items():
         cgID = NAME_ID_MAPPING.get(value["name"])
         asset = Asset(value["name"], value["symbol"], value["last_price"], cgID)
-        assets.append(asset)
+        if  asset.name in NAME_ID_MAPPING:
+            assets.append(asset)
     return assets
-    
 
 if __name__ == "__main__":
     energiAssets = parseEnergiData()
-    # for energiAsset in energiAssets:
-    #     if energiAsset.name in NAME_ID_MAPPING:
-    #         print(energiAsset)
+    coinGeckoAssets = parseCoinGeckoData()
+    for energiAsset in energiAssets:
+        print(energiAsset)
 
---
+++ executable/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    for energiAsset in energiAssets:
+        if energiAsset.name in NAME_ID_MAPPING:
+            print(energiAsset)
