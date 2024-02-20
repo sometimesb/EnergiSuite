a516e0eeacf7fed5c79b1e89f56efbd4f28bf11d
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-02-20 11:13:18 -0600

    finished energi parser, converts to cgID well.

--- executable/main.py
+++ executable/main.py
@@ -1,20 +1,37 @@
 from botasaurus import *
+from mapping import NAME_ID_MAPPING
+import json
 
-@request(use_stealth=True)
-def scrape_heading_task(request: AntiDetectRequests, data):
-    response = request.get('http://api.energiswap.exchange/v1/assets')
-    print(response.status_code)
+class Asset:
+    def __init__(self, name, symbol, price, cgID=None):
+        self.name = name
+        self.symbol = symbol
+        self.price = price
+        self.cgID = cgID
+
+    def __str__(self):
+        return f"{self.name} {self.cgID} {self.price}"
+
+@request(use_stealth=True, output="energi")
+def EnergiRequester(request: AntiDetectRequests, data):
+    response = request.get("https://api.energiswap.exchange/v1/assets")
+    if response.status_code != 200:
+        return "Error"
     return response.text
 
-def handleRequest(data):
-    pass
+def parseEnergiData():
+    data = json.loads(EnergiRequester())
+    assets = []
+    for key, value in data.items():
+        cgID = NAME_ID_MAPPING.get(value["name"])
+        asset = Asset(value["name"], value["symbol"], value["last_price"], cgID)
+        assets.append(asset)
+    return assets
+    
 
 if __name__ == "__main__":
-    apiReturn = scrape_heading_task()
-    print(apiReturn)
+    energiAssets = parseEnergiData()
+    # for energiAsset in energiAssets:
+    #     if energiAsset.name in NAME_ID_MAPPING:
+    #         print(energiAsset)
 
-
-
-
-
-    
---
+++ executable/mapping.py
@@ -0,0 +1,8 @@
+NAME_ID_MAPPING = {
+    "Wrapped NRG": "energi",
+    "Energi Dollar": "energi-dollar",
+    "Dai Stablecoin": "dai",
+    "Ether": "ethereum",
+    "Bitcoin": "bitcoin",
+    "USD Coin": "usd-coin",
+}
