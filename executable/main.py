235c5bb7b12f94f71a941730555b33fccabbee90
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-02-22 17:11:30 -0600

    ui almost completed. need to link the profit % slider into the script itself, as well as enable and disable coins.

--- executable/main.py
+++ executable/main.py
@@ -6,6 +6,7 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 from tkinter import ttk
+import colorama
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -39,28 +40,28 @@ class App(customtkinter.CTk):
         # create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
 
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Energi Suite", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Execute", command=self.execute)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Author Info", command=self.execute)
-        self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10)
+
+        self.running_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Running Mode:", anchor="w")
+        self.running_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.running_mode_menu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["USD Mode", "Crypto Mode"],command=self.change_appearance_mode_event)
+        self.running_mode_menu.grid(row=7, column=0, padx=20, pady=(10, 10))
 
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=8, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 10))
 
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=10, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
-
-        self.appearance_mode_optionemenu.set("Dark")
-        self.scaling_optionemenu.set("100%")
+        self.scaling_optionemenu.grid(row=11, column=0, padx=20, pady=(10, 20))
 
         # create textbox
         self.textbox = customtkinter.CTkTextbox(self, width=250)
@@ -68,31 +69,68 @@ class App(customtkinter.CTk):
         self.textbox.insert("0.0", "Execute to populate data.")
         self.textbox.configure(state="disabled")
 
-        # create slider and progressbar frame
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self,fg_color="transparent")
-        self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=5, number_of_steps=100)
-        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        #tabview
+        self.tabview = customtkinter.CTkTabview(self, width=250)
+        self.tabview.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.tabview.add("USDMode Parameters")
+        self.tabview.tab("USDMode Parameters").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.add("CryptoMode Parameters")
+        self.tabview.tab("CryptoMode Parameters").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
 
-        # self.user_profit_label = customtkinter.CTkLabel(self.slider_progressbar_frame, text="User Profit",font=customtkinter.CTkFont(size=15, weight="bold"),text_color="green", anchor="w")
-        # self.user_profit_label.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="w")  # Position above the slider
+        #first tabview
+        self.slider_1 = customtkinter.CTkSlider(self.tabview.tab("USDMode Parameters"), from_=0, to=10, number_of_steps=100,command=self.slider_event_1)
+        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(100, 100), sticky="ew")
 
+        self.text_var_r_1 = tkinter.StringVar(value="")
+        self.entry_1 = customtkinter.CTkLabel(self.tabview.tab("USDMode Parameters"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_1)
+        self.entry_1.grid(row=3, column=1, columnspan=1, padx=(5,5), pady=(5,5), sticky="ew")
+
+        #second tabview
+        self.text_var_r_2 = tkinter.StringVar(value="")
+        self.slider_2 = customtkinter.CTkSlider(self.tabview.tab("CryptoMode Parameters"), from_=0, to=5, number_of_steps=50,button_color="blue",command=self.slider_event_2)
+        self.slider_2.grid(row=3, column=0, padx=(20, 10), pady=(100, 100), sticky="ew")
+
+        self.entry_2 = customtkinter.CTkLabel(self.tabview.tab("CryptoMode Parameters"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_2)
+        self.entry_2.grid(row=3, column=1, columnspan=1, padx=(5,5), pady=(5,5), sticky="ew")
 
         # create scrollable frame
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="Enable & Disable Coins")
-        self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
-        for i in range(100):
-            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-            self.scrollable_frame_switches.append(switch)
+        self.scrollable_frame_USD = customtkinter.CTkScrollableFrame(self, label_text="USD Coin Selection")
+        self.scrollable_frame_USD.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame_USD.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_switches_USD = []
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+        # Create switches with specific values
+        switch_values = ["DAI", "USDC", "USDE"]
+        for value in switch_values:
+            switch = customtkinter.CTkSwitch(master=self.scrollable_frame_USD, text=value)
+            switch.grid(row=len(self.scrollable_frame_switches_USD), column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_switches_USD.append(switch)
+
+        self.scrollable_frame_CG = customtkinter.CTkScrollableFrame(self, label_text="Crypto Coin Selection")
+        self.scrollable_frame_CG.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame_CG.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_switches_CG = []
+
+        # Access key values from dictionary
+        coin_names = list(NAME_ID_MAPPING.keys())
+
+        for name in coin_names:
+            switch = customtkinter.CTkSwitch(master=self.scrollable_frame_CG, text=name)
+            switch.grid(row=len(self.scrollable_frame_switches_CG), column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_switches_CG.append(switch)
+
+        #set defaults
+        self.text_var_r_1.set(f"{round(float(self.slider_1.get()), 2)}")
+        self.text_var_r_2.set(f"{round(float(self.slider_2.get()), 2)}")
+
+        self.appearance_mode_optionemenu.set("Dark")
+        self.scaling_optionemenu.set("100%")
+
+    def slider_event_1(self,value):
+        self.text_var_r_1.set(f"{round(float(self.slider_1.get()), 1)}")
+
+    def slider_event_2(self,value):
+        self.text_var_r_2.set(f"{round(float(self.slider_2.get()), 1)}")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -108,6 +146,7 @@ class App(customtkinter.CTk):
         for asset in assets:
             print(asset)
             self.textbox.insert("0.0",asset)
+            self.textbox.insert("0.0","\n")
 
 def apiRequester(url,mode):
     @request(use_stealth=True, output="energi")
@@ -161,3 +200,5 @@ def getAssets():
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
+    
