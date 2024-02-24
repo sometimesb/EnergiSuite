8e7958d3b6b9fe99b946b165d928a49a0e6dfee7
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-02-24 16:27:36 -0600

    auto run looks to be completed, may be an issue where a thread doesnt die...looking into it.

--- executable/main.py
+++ executable/main.py
@@ -6,6 +6,8 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 from tkinter import ttk
+import threading
+import time
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -21,7 +23,6 @@ class Asset:
     def __str__(self):
         return f"{self.name} {self.cgID} {self.price} {self.cgPrice}"
 
-#UI
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -46,6 +47,7 @@ class App(customtkinter.CTk):
 
     def create_sidebar_frame(self):
         """Creates the sidebar frame with its widgets."""
+        self.auto_execute_status = False  # Initialize auto_execute_status
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
@@ -59,11 +61,10 @@ class App(customtkinter.CTk):
         self.running_mode_menu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["USD Mode", "Crypto Mode"], command=self.running_mode_event)
         self.running_mode_menu.grid(row=7, column=0, padx=20, pady=(10, 10))
 
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Execute", command=lambda: self.execute(self.running_mode_menu.get()))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Execute", command=lambda: threading.Thread(target=self.execute, args=(self.running_mode_menu.get(),)).start())
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
 
-        #TODO - Fix this
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Start Auto Execute", command=lambda: self.AutoRunDialoge())
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Start Auto Execute", command=self.toggle_auto_execute)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
 
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
@@ -87,26 +88,36 @@ class App(customtkinter.CTk):
         #tabview
         self.tabview = customtkinter.CTkTabview(self, width=250)
         self.tabview.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("USDMode Parameters")
-        self.tabview.tab("USDMode Parameters").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.add("CryptoMode Parameters")
-        self.tabview.tab("CryptoMode Parameters").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.add("USDMode")
+        self.tabview.tab("USDMode").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.add("CryptoMode")
+        self.tabview.tab("CryptoMode").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.add("AutoRun Timer")
+        self.tabview.tab("AutoRun Timer").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
 
         #first tabview
-        self.slider_1 = customtkinter.CTkSlider(self.tabview.tab("USDMode Parameters"), from_=0, to=10, number_of_steps=100,command=self.slider_event_1)
+        self.slider_1 = customtkinter.CTkSlider(self.tabview.tab("USDMode"), from_=0, to=10, number_of_steps=100,command=self.slider_event_1)
         self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(100, 100), sticky="ew")
 
         self.text_var_r_1 = tkinter.StringVar(value="")
-        self.entry_1 = customtkinter.CTkLabel(self.tabview.tab("USDMode Parameters"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_1)
+        self.entry_1 = customtkinter.CTkLabel(self.tabview.tab("USDMode"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_1)
         self.entry_1.grid(row=3, column=1, columnspan=1, padx=(5,5), pady=(5,5), sticky="ew")
 
         #second tabview
         self.text_var_r_2 = tkinter.StringVar(value="")
-        self.slider_2 = customtkinter.CTkSlider(self.tabview.tab("CryptoMode Parameters"), from_=0, to=10, number_of_steps=100,button_color="blue",button_hover_color="blue",command=self.slider_event_2)
+        self.slider_2 = customtkinter.CTkSlider(self.tabview.tab("CryptoMode"), from_=0, to=10, number_of_steps=100,button_color="blue",button_hover_color="blue",command=self.slider_event_2)
         self.slider_2.grid(row=3, column=0, padx=(20, 10), pady=(100, 100), sticky="ew")
 
-        self.entry_2 = customtkinter.CTkLabel(self.tabview.tab("CryptoMode Parameters"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_2)
+        self.entry_2 = customtkinter.CTkLabel(self.tabview.tab("CryptoMode"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_2)
         self.entry_2.grid(row=3, column=1, columnspan=1, padx=(5,5), pady=(5,5), sticky="ew")
+
+        #third tabview
+        self.text_var_r_3 = tkinter.StringVar(value="")
+        self.slider_3 = customtkinter.CTkSlider(self.tabview.tab("AutoRun Timer"), from_=10, to=60, number_of_steps=50,button_color="red",button_hover_color="red",command=self.slider_event_3)
+        self.slider_3.grid(row=3, column=0, padx=(20, 10), pady=(100, 100), sticky="ew")
+
+        self.entry_3 = customtkinter.CTkLabel(self.tabview.tab("AutoRun Timer"),font=customtkinter.CTkFont(size=15),corner_radius=8,text_color="white",textvariable=self.text_var_r_3)
+        self.entry_3.grid(row=3, column=1, columnspan=1, padx=(5,5), pady=(5,5), sticky="ew")
 
     def create_scrollable_frames(self):
         """Creates scrollable frames for USD and Crypto coin selections with switches."""
@@ -137,6 +148,7 @@ class App(customtkinter.CTk):
 
         self.text_var_r_1.set(f"{round(float(self.slider_1.get()), 1)}%")
         self.text_var_r_2.set(f"{round(float(self.slider_2.get()), 2)}%")
+        self.text_var_r_3.set(f"{round(int(self.slider_3.get()), 2)}s")
 
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
@@ -145,7 +157,7 @@ class App(customtkinter.CTk):
 
     def running_mode_event(self, mode):
         if mode == "USD Mode":
-            self.tabview.set("USDMode Parameters")
+            self.tabview.set("USDMode")
             self.slider_2.configure(state="disabled")
             self.slider_1.configure(state="normal")
             for self.switch in self.scrollable_frame_switches_CG:
@@ -157,7 +169,7 @@ class App(customtkinter.CTk):
             self.slider_1.configure(state="disabled")
             self.slider_2.configure(state="normal")
 
-            self.tabview.set("CryptoMode Parameters")
+            self.tabview.set("CryptoMode")
             for self.switch in self.scrollable_frame_switches_USD:
                 self.switch.configure(state="disabled")
                 self.switch.toggle()
@@ -173,6 +185,9 @@ class App(customtkinter.CTk):
 
     def slider_event_2(self,value):
         self.text_var_r_2.set(f"{round(float(self.slider_2.get()), 1)}%")
+    
+    def slider_event_3(self,value):
+        self.text_var_r_3.set(f"{round(int(self.slider_3.get()), 1)}s")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -181,17 +196,54 @@ class App(customtkinter.CTk):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
     
-    #TODO: FIX THIS
-    def AutoRunDialoge(self):
-        self.dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="Test")
-        print("Number:", self.dialog.get_input())
+    def toggle_auto_execute(self):
+        """Toggle AutoExecute on or off."""
+        if self.auto_execute_status:
+            # Stop AutoExecute
+            self.auto_execute_status = False
+            self.sidebar_button_2.configure(text="Start Auto Execute", command=self.toggle_auto_execute)
+            self.textbox.configure(state="normal")
+            self.textbox.delete(0.0, 'end')
+            self.textbox.insert("0.0", "Stopped auto execution.")
+            self.textbox.configure(state="disabled")
+            for self.switch in self.scrollable_frame_switches_CG:
+                self.switch.configure(state="normal")  
+            for self.switch in self.scrollable_frame_switches_USD:
+                self.switch.configure(state="normal") 
+        else:
+            # Start AutoExecute
+            self.auto_execute_status = True
+            threading.Thread(target=self.AutoExecute, args=(self.running_mode_menu.get(),)).start()
+            self.sidebar_button_2.configure(text="Stop Auto Execute", command=self.toggle_auto_execute)
+    
+    def AutoExecute(self, runningMode):
+        timer_value = int(self.text_var_r_3.get().replace("s", ""))
+        sleep_time = timer_value - time.time() % timer_value  # Calculate sleep time once
+        
+        while self.auto_execute_status:
+
+            for self.switch in self.scrollable_frame_switches_CG:
+                self.switch.configure(state="disabled")  
+            for self.switch in self.scrollable_frame_switches_USD:
+                self.switch.configure(state="disabled") 
+
+            self.execute(runningMode)  # Execute the search and update textbox
+            # Update timer and text in the textbox
+            self.textbox.configure(state="normal")
+            self.textbox.insert("0.0", f"Timer: {timer_value}s\n")
+            self.textbox.configure(state="disabled")
+
+            # Sleep for the specified timer interval, minus the time spent in the loop
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+            sleep_time = timer_value  # Update sleep time for the next iteration
 
     def execute(self, runningMode):
         self.textbox.configure(state="normal")
-        self.textbox.delete(0.0, 'end')
 
         assets = self.getAssets(runningMode)
         any_assets_added = False
+        self.textbox.delete(0.0, 'end')
 
         for asset in assets:
             if (runningMode == "Crypto Mode") and float(self.text_var_r_2.get().replace('%', '')) < self.profitPercent(asset.price, asset.cgPrice):
@@ -206,7 +258,6 @@ class App(customtkinter.CTk):
 
         self.textbox.configure(state="disabled")
 
-    #SCRAPING
     def profitPercent(self, energiPrice, cgPrice):
         if cgPrice is None or energiPrice is None:
             return None  
@@ -247,7 +298,14 @@ class App(customtkinter.CTk):
         return base_url
 
     def getAssets(self,runningMode):
-        energiData = json.loads(self.apiRequester("https://api.energiswap.exchange/v1/assets", 0))
+        try:
+            energiData = json.loads(self.apiRequester("https://api.energiswap.exchange/v1/assets", 0))
+        except:
+            self.textbox.configure(state="normal")
+            self.textbox.delete(0.0, 'end')
+            self.textbox.insert("0.0", "API Limiter hit, please wait 60 seconds for EnergiSwap to unblock.\n")
+            self.textbox.configure(state="disabled")
+
         switch_states = {}
                 
         if runningMode == "Crypto Mode":
